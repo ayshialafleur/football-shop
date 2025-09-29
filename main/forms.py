b57d5f37fa988e9f3ca8ddd3bc29eefcfebce205
ayshialafleur <ayshialafleur@gmail.com>
2025-09-29 23:03:46 +0700

    completed assignment 5

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,9 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 from main.models import Product
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "description", "thumbnail", "category", "is_featured"]     
+        fields = ["name", "price", "description", "thumbnail", "category", "is_featured"]
+
+         
